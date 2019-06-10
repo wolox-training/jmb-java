9886b6aec12d1b9b16e5648e97d456cbf746b266
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-06-10 14:20:29 -0300

    Add the User repository
    - Include a getByUsername method

---
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -0,0 +1,22 @@
+package wolox.training.repositories;
+
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import wolox.training.models.User;
+
+/**
+ * A repository for {@link User}s.
+ */
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    /**
+     * Returns one {@link User} with the given {@code username}
+     *
+     * @param username The username used to search for a {@link User}.
+     * @return An {@link Optional} containing a {@link User} with the given {@code username} if
+     * there is such, or empty otherwise.
+     */
+    Optional<User> getByUsername(final String username);
+}
