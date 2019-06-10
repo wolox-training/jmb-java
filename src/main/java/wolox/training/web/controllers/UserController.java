5aac3a296cfc7ccde344f63580b96cb6baacf78e
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-06-10 15:27:54 -0300

    Add books add/remove capabilities to UserController

--- src/main/java/wolox/training/web/controllers/UserController.java
+++ src/main/java/wolox/training/web/controllers/UserController.java
@@ -1,5 +1,7 @@
 package wolox.training.web.controllers;
 
+import java.util.Set;
+import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +13,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import wolox.training.models.Book;
 import wolox.training.models.User;
+import wolox.training.repositories.BookRepository;
 import wolox.training.repositories.UserRepository;
 import wolox.training.web.dtos.UserCreationRequestDto;
 import wolox.training.web.dtos.UserDownloadDto;
@@ -32,15 +37,25 @@ public class UserController {
      */
     private final UserRepository userRepository;
 
+    /**
+     * The {@link BookRepository} used to search for {@link Book}s when they need to be added to a
+     * {@link User}'s {@link Book} list.
+     */
+    private final BookRepository bookRepository;
+
 
     /**
      * Constructor.
      *
      * @param userRepository The {@link UserRepository} used to read and write {@link User} data.
+     * @param bookRepository The {@link BookRepository} used to search for {@link Book}s when they
+     * need to be added to a {@link User}'s {@link Book} list.
      */
     @Autowired
-    public UserController(final UserRepository userRepository) {
+    public UserController(final UserRepository userRepository,
+        final BookRepository bookRepository) {
         this.userRepository = userRepository;
+        this.bookRepository = bookRepository;
     }
 
 
@@ -59,22 +74,22 @@ public class UserController {
     }
 
     /**
-     * Endpoint for getting a {@link User} by its {@code id}.
+     * Endpoint for getting a {@link User} by its {@code userId}.
      *
-     * @param id The id of the {@link User} to be retrieved.
-     * @return A {@link ResponseEntity} containing the {@link User} with the given {@code id} if it
-     * exists, or with 404 Not Found {@link ResponseEntity} otherwise.
+     * @param userId The id of the {@link User} to be retrieved.
+     * @return A {@link ResponseEntity} containing the {@link User} with the given {@code userId} if
+     * it exists, or with 404 Not Found {@link ResponseEntity} otherwise.
      */
-    @GetMapping("/{id}")
-    public ResponseEntity<UserDownloadDto> getById(@PathVariable("id") final long id) {
-        return userRepository.findById(id)
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDownloadDto> getById(@PathVariable("userId") final long userId) {
+        return userRepository.findById(userId)
             .map(UserDownloadDto::new)
             .map(ResponseEntity::ok)
             .orElse(ResponseEntity.notFound().build());
     }
 
     /**
-     * Creates a {@link User} according to the given {@link UserCreationRequestDto}.
+     * Endpoint for creating a {@link User} according to the given {@link UserCreationRequestDto}.
      *
      * @param dto The {@link UserCreationRequestDto} with needed data to create a {@link User}
      * @return A {@link ResponseEntity} containing the created {@link User}. The response will
@@ -97,16 +112,98 @@ public class UserController {
     }
 
     /**
-     * Deletes the {@link User} with the given {@code id}.
+     * Endpoint for deleting the {@link User} with the given {@code userId}.
      *
-     * @param id The id of the {@link User} to be deleted.
+     * @param userId The id of the {@link User} to be deleted.
      * @return A 204 No Content {@link ResponseEntity}.
      */
-    @DeleteMapping("/{id}")
-    public ResponseEntity deleteUser(@PathVariable("id") final long id) {
-        if (userRepository.existsById(id)) {
-            userRepository.deleteById(id);
+    @DeleteMapping("/{userId}")
+    public ResponseEntity deleteUser(@PathVariable("userId") final long userId) {
+        if (userRepository.existsById(userId)) {
+            userRepository.deleteById(userId);
         }
         return ResponseEntity.noContent().build();
+    }
+
+    /**
+     * Endpoint for getting the {@link Book}s of the {@link User} with the given {@code userId}.
+     *
+     * @param userId The id of the {@link User} whose {@link Book}s will be returned.
+     * @return A {@link ResponseEntity} of {@link Iterable} of {@link Book}s that belong to the
+     * {@link User} with the given {@code userId}. Might be empty.
+     */
+    @GetMapping("/{userId}/books")
+    public ResponseEntity<Set<Book>> getUserBooks(@PathVariable("userId") final long userId) {
+        return userRepository.findById(userId)
+            .map(User::getBooks)
+            .map(books -> {
+                books.size(); // Initialize lazy collection
+                return ResponseEntity.ok(books);
+            })
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    /**
+     * Endpoint for adding the {@link Book} with the given {@code bookId} to the {@link User} with
+     * the given {@code userId}.
+     *
+     * @param userId The id of the {@link User}.
+     * @param bookId The id of the {@link Book}.
+     * @return A 204 No Content {@link ResponseEntity} if the {@link Book} could be added without
+     * problem, or a 404 Not Found {@link ResponseEntity} if the {@link User} or the {@link Book}
+     * does not exist.
+     */
+    @PutMapping("/{userId}/books/{bookId}")
+    @Transactional
+    public ResponseEntity<?> addBook(
+        @PathVariable("userId") final long userId,
+        @PathVariable("bookId") final long bookId) {
+        return operateOverBookAndUser(userId, bookId, User::addBook);
+    }
+
+    /**
+     * Endpoint for removing the {@link Book} with the given {@code bookId} from the {@link User}
+     * with the given {@code userId}.
+     *
+     * @param userId The id of the {@link User}.
+     * @param bookId The id of the {@link Book}.
+     * @return A 204 No Content {@link ResponseEntity} if the {@link Book} could be removed without
+     * problem, or a 404 Not Found {@link ResponseEntity} if the {@link User} or the {@link Book}
+     * does not exist.
+     * @apiNote This is an idempotent operation (i.e removing a {@link Book} that is not present in
+     * the {@link User}'s list does not cause any error).
+     */
+    @DeleteMapping("/{userId}/books/{bookId}")
+    @Transactional
+    public ResponseEntity removeBook(
+        @PathVariable("userId") final long userId,
+        @PathVariable("bookId") final long bookId) {
+        return operateOverBookAndUser(userId, bookId, User::removeBook);
+    }
+
+    /**
+     * Performs an operation over the {@link User} and {@link Book} with the given {@code userId}
+     * {@code bookId}, according to the given {@code operation} {@link BiConsumer}.
+     *
+     * @param userId The id of the {@link User}.
+     * @param bookId The id of the {@link Book}.
+     * @param operation A {@link BiConsumer} of {@link User} and {@link Book} to which the operation
+     * is delegated.
+     * @return A 204 No Content {@link ResponseEntity} if the {@code operation} could be performed
+     * without problem, or a 404 Not Found {@link ResponseEntity} in case the {@link User} or the
+     * {@link Book} do not exist.
+     */
+    private ResponseEntity operateOverBookAndUser(
+        final long userId,
+        final long bookId,
+        final BiConsumer<User, Book> operation) {
+        return userRepository.findById(userId)
+            .flatMap(user ->
+                bookRepository.findById(bookId)
+                    .map(book -> {
+                        operation.accept(user, book);
+                        return ResponseEntity.noContent().build();
+                    }))
+            .orElse(ResponseEntity.notFound().build());
     }
 }
