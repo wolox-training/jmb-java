2580697da15ae1bb626be61ced48824b2140c15c
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-06-28 04:30:32 -0300

    Add custom queries to the repositories
    - They allow optional filters

--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package wolox.training.repositories;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import wolox.training.models.User;
 
@@ -37,4 +39,25 @@ public interface UserRepository extends CrudRepository<User, Long> {
         final LocalDate to,
         final String namePattern
     );
+
+    /**
+     * Same as {@link #getByBirthDateBetweenAndNameContainingIgnoreCase(LocalDate, LocalDate,
+     * String)}, but with optional parameters.
+     *
+     * @param from The min. {@link LocalDate} for the birth date.
+     * @param to The max. {@link LocalDate} for the birth date.
+     * @param namePattern The pattern to be matched in the name.
+     * @return The {@link User}s matching the criteria.
+     */
+    @Query(
+        value = "SELECT u"
+            + " FROM User u"
+            + " WHERE ( :lowerDate IS NULL OR u.birthDate > :lowerDate )"
+            + " AND ( :upperDate IS NULL OR u.birthDate < :upperDate )"
+            + " AND ( :namePattern IS NULL OR upper(u.name) LIKE upper(concat('%', :namePattern, '%')) )"
+    )
+    List<User> getWithBirthDateAndName(
+        @Param("lowerDate") final LocalDate from,
+        @Param("upperDate") final LocalDate to,
+        @Param("namePattern") final String namePattern);
 }
